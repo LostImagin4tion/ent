558711d74afbfad669871a3ac30e9f21997e593a
danilov6083 <danilov6083@outlook.com>
2026-01-16 15:10:07 +0300

    dialect: removed query mode and enabled query service for database/sql driver

--- dialect/sql/schema/atlas.go
+++ dialect/sql/schema/atlas.go
@@ -24,7 +24,6 @@ import (
 	"entgo.io/ent/dialect"
 	entsql "entgo.io/ent/dialect/sql"
 	"entgo.io/ent/schema/field"
-	"github.com/ydb-platform/ydb-go-sdk/v3"
 )
 
 // Atlas atlas migration engine.
@@ -634,7 +633,7 @@ func (a *Atlas) create(ctx context.Context, tables ...*Table) (err error) {
 			func(ctx context.Context, conn dialect.ExecQuerier, plan *migrate.Plan) error {
 				for _, c := range plan.Changes {
 					err := conn.Exec(
-						ydb.WithQueryMode(ctx, ydb.SchemeQueryMode),
+						ctx,
 						c.Cmd,
 						c.Args,
 						nil,
